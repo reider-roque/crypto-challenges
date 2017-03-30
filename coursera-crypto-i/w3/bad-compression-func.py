1742848a0d8a55713d25c1159e456d5033d9485c
Oleg Mitrofanov <oleg@wryway.com>
2017-03-30 07:54:06 -0700

    Add collision finding solution to the bad hash function problem

--- coursera-crypto-i/w3/bad-compression-func.py
+++ coursera-crypto-i/w3/bad-compression-func.py
@@ -1,6 +1,12 @@
 # Crypto I. Week 3. Constructing Compression Functions. HW assignment from video at 5:00.
 # Tested in Python 3
 
+# The collision finding solution:
+# E(k1, m1) ^ k1 = E(k2, m2) ^ k2
+# E(k1, m1) ^ k1 ^ k2 = E(k2, m2)
+# D(k2, (E(k1, m1) ^ k1 ^ k2)) = D(k2, (E(k2, m2)))
+# D(k2, (E(k1, m1) ^ k1 ^ k2)) = m2
+
 from Crypto.Cipher import AES
 
 
